61511807918f069e4d46b7aa2bf809ae79de647b
bfzheng <baofeng_zheng@163.com>
2018-09-06 00:21:36 +0800

    finish over 30 minus 6 discount

--- src/best-charge.js
+++ src/best-charge.js
@@ -4,7 +4,7 @@ let total = 0;
 
 function bestCharge(selectedItems) {
   let items = inputTransferToItems(selectedItems);
-  return print(items);
+  return printHeader() + printItems(items) + printMiddle() + overThirtyMinusSixDiscount() + printTotal(total) + printFooter();
 }
 
 const print = (items) => {
@@ -13,9 +13,10 @@ const print = (items) => {
 
 const printItems = (items) => {
   let res = '';
+  total = 0;
   for (let i = 0; i < items.length; i++) {
     let subTotal = items[i].price * items[i].count;
-    res += items[i].name + ' x ' + items[i].count + ' = ' + subTotal + '元' + '\n';
+    res += items[i].name + " x " + items[i].count + " = " + subTotal + "元\n";
     total += subTotal;
   }
   return res;
@@ -30,12 +31,20 @@ const printMiddle = () => {
 }
 
 const printTotal = (total) => {
-  return '总计：' + total + '元';
+  return "总计：" + total + "元\n";
 }
 
 const printFooter = () => {
   return "===================================\n";
 }
 
+const overThirtyMinusSixDiscount = () => {
+  if (total < 30) {
+    return "";
+  }
+  let res = "使用优惠:\n满30减6元，省6元\n-----------------------------------\n";
+  total -= Math.round(total / 30) * 6;
+  return res;
+}
 
 module.exports = bestCharge;
