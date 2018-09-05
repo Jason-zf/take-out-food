216f9281f1215da16a049f32d321680bf6f5e1f3
bfzheng <baofeng_zheng@163.com>
2018-09-06 00:05:40 +0800

    finish output selectedItems total price without no promotion

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,41 @@
-function bestCharge(selectedItems) {
+const inputTransferToItems = require('../src/items');
 
+let total = 0;
+
+function bestCharge(selectedItems) {
+  let items = inputTransferToItems(selectedItems);
+  return print(items);
 }
+
+const print = (items) => {
+  return printHeader() + printItems(items) + printMiddle() + printTotal(total) + printFooter();
+}
+
+const printItems = (items) => {
+  let res = '';
+  for (let i = 0; i < items.length; i++) {
+    let subTotal = items[i].price * items[i].count;
+    res += items[i].name + ' x ' + items[i].count + ' = ' + subTotal + '元' + '\n';
+    total += subTotal;
+  }
+  return res;
+}
+
+const printHeader = () => {
+  return "============= 订餐明细 =============\n";
+}
+
+const printMiddle = () => {
+  return "-----------------------------------\n";
+}
+
+const printTotal = (total) => {
+  return '总计：' + total + '元';
+}
+
+const printFooter = () => {
+  return "===================================\n";
+}
+
+
+module.exports = bestCharge;
